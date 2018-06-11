1d09e54e1c33afd4650e2895fb1ac8c8c0a277ba
teppanm1 <39699330+teppanm1@users.noreply.github.com>
2018-06-11 13:47:40 +0200

    Update MainGui.java
    btn

--- ForDelition/src/pkgGui/MainGui.java
+++ ForDelition/src/pkgGui/MainGui.java
@@ -38,6 +38,7 @@ public class MainGui extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		new JButton btnHello;
 	}
 
 }
